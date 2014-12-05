71fad104788fc40fb2fc5655e2806b7451a7dbc1
Jiangge Zhang <tonyseek@gmail.com>
2014-12-05 19:27:07 +0800

    implement a simple version.

--- base36.py
+++ base36.py
@@ -1,1 +1,44 @@
+import sys
+
+
 __version__ = '0.1.0'
+__all__ = ['dumps', 'loads']
+
+
+if sys.version_info.major == 2:
+    integer_types = (int, long)
+else:
+    integer_types = (int,)
+
+
+alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+
+def dumps(number):
+    """Dumps an integer into a base36 string.
+
+    :param number: the 10-based integer.
+    :returns: the base36 string.
+    """
+    if not isinstance(number, integer_types):
+        raise TypeError('number must be an integer')
+
+    if number < 0:
+        return '-' + dumps(-number)
+
+    value = ''
+
+    while number != 0:
+        number, index = divmod(number, len(alphabet))
+        value = alphabet[index] + value
+
+    return value
+
+
+def loads(value):
+    """Loads a base36 string and parse it into 10-based interger.
+
+    :param value: the base36 string.
+    :returns: the parsed integer.
+    """
+    return int(value, 36)
---
+++ test_base36.py
@@ -0,0 +1,14 @@
+import pytest
+import base36
+
+
+@pytest.mark.parametrize('number,value', [
+    (3951668550778163018, 'u0tplaqiv70q'),
+    (1024, 'sg'),
+    (1843067821, 'uhbc8d'),
+])
+def test_dumps_and_loads(number, value):
+    assert base36.dumps(number) == value
+    assert base36.dumps(-number) == '-' + value
+    assert base36.loads(value) == number
+    assert base36.loads('-' + value) == -number
