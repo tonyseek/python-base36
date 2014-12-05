9574bdf2794233a36534d78eabd51640df9955d6
Jiangge Zhang <tonyseek@gmail.com>
2014-12-05 17:47:01 +0800

    initial commit.

---
+++ README.rst
@@ -0,0 +1,4 @@
+Base36 in Python
+================
+
+Yet another implementation for the positional numeral system using 36 as the radix.
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+with open('README.rst') as readme:
+    long_description = ''.join(readme).strip()
+
+
+setup(
+    name='base36',
+    version='0.1.0',
+    author='Jiangge Zhang',
+    author_email='tonyseek@gmail.com',
+    description='Yet another implementation for the positional numeral system '
+                'using 36 as the radix.',
+    long_description=long_description,
+    url='https://github.com/tonyseek/python-base36',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
