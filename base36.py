65aa438c7e0297b7edb5f729f3181e510f87efdf
Jiangge Zhang <tonyseek@gmail.com>
2014-12-07 18:16:52 +0800

    Bump version: 0.1.0 → 0.1.1

--- base36.py
+++ base36.py
@@ -1,7 +1,7 @@
 import sys
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = ['dumps', 'loads']
 
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as readme:
 
 setup(
     name='base36',
-    version='0.1.0',
+    version='0.1.1',
     author='Jiangge Zhang',
     author_email='tonyseek@gmail.com',
     description='Yet another implementation for the positional numeral system '
